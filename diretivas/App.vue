e9e25094664e14b2e44445c1dd653a859fcf6412
Ismael <ismaelirc@gmail.com>
2021-01-17 11:53:03 -0300

    Sbeginning of my vue js studies

---
+++ diretivas/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Minha lista</h1>
+    <button @click="() => showList = !showList" >Ver lista</button>
+
+    <ul v-if="showList">
+        <li
+            v-for="(task, index) in tasks" 
+            :key="`${task}-${index}`"
+        >
+            {{task.name}}
+        </li>
+    </ul>
+    <p v-else>Lista de tarefas escondidas</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        showList: false,
+        tasks:[
+            {name: 'Fazer o curso', isDone: false}
+        ]
+    })
+}
+</script>
